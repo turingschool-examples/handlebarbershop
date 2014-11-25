5f554ede30b8cb7dbbb5fc0376e2f34b436681bf
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-11-24 17:24:05 -0700

    reset to ERB

--- handlebarbershop.rb
+++ handlebarbershop.rb
@@ -1,29 +1,27 @@
-require 'sinatra/handlebars'
-
 class Handlebarbershop < Sinatra::Base
-  helpers Sinatra::Handlebars
 
   get '/' do
-    handlebars :simple, locals: {name: 'Rachel'}
+    name = "Jennifer Johnson"
+    erb :index, locals: { name: name }
   end
 
   get '/shopping-list' do
-    foods =  [ 'Red apples', 'Green apples', 'Tan bars' ]
-    handlebars :iterate, locals: {foods: foods}
+    foods =  [ 'Red apples', 'Green apples', 'Yellow apples' ]
+    erb :shopping_list, locals: { foods: foods }
   end
 
   get '/pets' do
     pets = [
       Animal.new('Marley', 'Dog', 6),
       Animal.new('Bucu', 'Cat', 13),
-      Animal.new('Baby Elmo', 'Stuffed Animal', 2)
+      Animal.new('Ollie', 'Elephant', 2)
     ]
-    handlebars :properties, locals: {pets: pets}
+    erb :pets, locals: { pets: pets }
   end
 
   get '/pet' do
-    pet = Animal.new('Marley', 'Dog', 6)
-    handlebars :with, locals: {pet: pet}
+    pet = Animal.new('Joe', 'Red Panda', 3)
+    erb :pet, locals: { pet: pet }
   end
 
   run! if app_file == $0
